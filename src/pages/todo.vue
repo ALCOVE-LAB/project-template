9c91f96feb10ccdace9c683825948e6a4256be56
ice <ice@3njoy.tech>
2024-10-28 23:29:33 +0800

    update

---
+++ src/pages/todo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex-col gap-1 mx-auto max-w-3xl p-4">
+    <h2 class="text-5 font-bold leading-loose">TODO</h2>
+    <ol class="list-decimal ml-4">
+      <li>Font paragraph</li>
+      <li>Button Variant</li>
+      <li>
+        <span class="line-through mr-2">Number Input</span>
+        <span>Oct 28 2024 23:28:46</span>
+      </li>
+      <li>
+        <span class="line-through">Add Toast</span>
+        <span>Oct 28 2024 23:28:46</span>
+      </li>
+      <li>Optimize `component` page</li>
+      <li>Rebuild `table` by `div`</li>
+      <li>SVG animation</li>
+    </ol>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="less" scoped></style>
+
